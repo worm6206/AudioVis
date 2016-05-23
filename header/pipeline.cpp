9988ec369c5b33ecae2029a9cb7080dea3052aa4
Kevin Yen <kevinyen@kevin.local>
2016-05-22 20:42:17 -0400

    concept is more clear, much more work to do

--- header/pipeline.cpp
+++ header/pipeline.cpp
@@ -3,20 +3,50 @@
 #include <string>
 #include <sstream>
 #include "pipeline.hpp"
+#include <essentia/algorithmfactory.h>
+#include <essentia/streaming/algorithms/poolstorage.h>
+#include <essentia/streaming/algorithms/vectorinput.h>
+#include <essentia/scheduler/network.h>
 
 using namespace std;
+using namespace essentia;
+using namespace essentia::streaming;
+using namespace essentia::scheduler;
+
+/**
+
+	TODO:
+	- connection recognize
+	- dynamic factor create
+
+ */
+
+
+void pipelineReader::initAlgorithm(){
+
+	essentia::init();
+	streaming::AlgorithmFactory& factory = streaming::AlgorithmFactory::instance();
+
+	for(int x =0;x<this->parameters.size();x++){
+		Algorithm* temp = factory.create(this->parameters[x][0]);
+		this->Algorithms.push_back(temp);
+	}
+
+	// for(int x =0;x<this->parameters.size();x++){
+	// 	connect();
+	// }
+	connect(Algorithms[0]->output("audio"), Algorithms[1]->input("signal"));
+
+	Network(Algorithms[0]).run();
+}
 
 void pipelineReader::readFile(string fileName){
 	vector<string> file;
 	ifstream infile(fileName);
-	string line, delimiter = " -> ";
+	string line;
 
 	// file -> vector "file"
 	while (getline(infile, line)) file.push_back(line);
-
-	/*==============================================
-	=            REST - READ PARAMETERS            =
-	==============================================*/
 	
 	for(int i =0;i<file.size();i++){
 		if(file[i][0]>=65&&file[i][0]<=90){
@@ -32,7 +62,5 @@ void pipelineReader::readFile(string fileName){
 			this->parameters.push_back(temp);
 		}
 	}
-	
-	/*=====  End of REST - READ PARAMETERS  ======*/
 	
 }
--- src/Full.cpp
+++ src/Full.cpp
@@ -1,34 +1,53 @@
-/*
-A bandpass filter is an electronic device or 
-circuit that allows signals between two specific 
-frequencies to pass, but that discriminates against 
-signals at other frequencies.
-*/
+/**
+ *
+ * Main cpp for Essentia parsing.
+ * by Kevin Yen.
+ *
+ */
+
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
 #include <essentia/algorithmfactory.h>
 #include <essentia/streaming/algorithms/poolstorage.h>
 #include <essentia/streaming/algorithms/vectorinput.h>
 #include <essentia/scheduler/network.h>
 #include "../header/pipeline.hpp"
-// #include "credit_libav.h" 
 using namespace std;
 using namespace essentia;
 using namespace essentia::streaming;
 using namespace essentia::scheduler;
 
 int main(int argc, char* argv[]) {
+
 	pipelineReader pl;
+
+	/*----------  read file  ----------*/
 	pl.readFile("../pl/bandpass.pl");
-	cout << "ALG:" << endl;
-	for (int x =0;x<pl.parameters.size();x++){
-		cout << pl.parameters[x][0] << endl;
-	}
-	for (int x =0;x<pl.parameters.size();x++){
-		cout << "PARA" << x << ":" <<endl;
-		for(int y=1;y<pl.parameters[x].size();y++){
-			cout << pl.parameters[x][y] << endl;
-		}
-	}
+	pl.initAlgorithm();
+
+	// cout << "ALG:" << endl;
+	// for (int x =0;x<pl.parameters.size();x++){
+	// 	cout << x << ": " << pl.parameters[x][0] << endl;
+	// }
+	// for (int x =0;x<pl.parameters.size();x++){
+	// 	cout << "PARA" << x << ":" <<endl;
+	// 	for(int y=1;y<pl.parameters[x].size();y++){
+	// 		cout << x << ", " << y << ": " << pl.parameters[x][y] << endl;
+	// 	}
+	// }
+
+	/*----------  essentia  ----------*/
+	
+	// essentia::init();
+	// Pool pool;
+	// streaming::AlgorithmFactory& factory = streaming::AlgorithmFactory::instance();
+	// vector<Algorithm*> Algorithms;
+
+
+
+
+
 	return 0;
 }
